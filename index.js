6524ee51374f39938e1561720b1cd52b6b3a8bc6
Nzuki <mbulajay83@gmail.com>
2024-08-06 14:38:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors(
+    {origin: 'http://localhost:5173'}
+));
+
+// Read db.json
+const getData = () => {
+  const data = fs.readFileSync('db.json');
+  return JSON.parse(data);
+};
+
+// CRUD routes
+app.get('/blogs', (req, res) => {
+  const data = getData();
+  res.json(data.blogs);
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
